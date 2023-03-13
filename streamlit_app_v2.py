64e0c5b9b5c873fb67203bef84173b1698b341a2
Rhys-McAlister <107469648+Rhys-McAlister@users.noreply.github.com>
2023-03-13 21:32:13 +1100

    Update streamlit_app_v2.py

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -32,7 +32,6 @@ class IngestDataFromTlc:
 
 
 
-
 st.write("Select the year and month you would like to examine (this may take a few seconds to load).")
 
 year_selection = st.radio(
@@ -52,46 +51,29 @@ Ingest = IngestDataFromTlc()
 
 Ingest.read_taxi_data_from_tlc(year_selection, month_selection, "yellow")
 
+st.write(Ingest.data.head())
 
 Ingest.create_target_variable()
 
-
-st.write('Raw taxi data downloaded from NYC TLC website:')
-st.write(Ingest.data.head(5))
-
-# class EDAViz:
-
-#     def __init__(self, data = Ingest.data):
-#         self.data = data
-
-#     def plot_trip_duration(self):
-#         fig = px.histogram(self.data, x="trip_duration")
-#         st.plotly_chart(fig)
-
-# EDAViz = EDAViz()
-# EDAViz.plot_trip_duration()
-
-# class CleanData:
+class EDAViz:
     
-#     def __init__(self, data = Ingest.data):
-#         self.data = data
+    def __init__(self, data):
+        self.data = Ingest.data
+
+    def plot_trip_duration(self):
+        fig = px.histogram(self.data, x="trip_duration")
+        st.plotly_chart(fig)
+        
+
+EDAViz = EDAViz(Ingest.data)
+st.write(EDAViz.data.head())
 
 
-#     # # Count the number of null values in each column
-#     # def count_null_values(self):
-#     #     return self.data.null_count()
-    
-# cleandata = CleanData()
-# st.write(cleandata.data)
 
 
-# class FeatureEngineering:
-#     data: pl.DataFrame = Clean.data
+# st.write('Raw taxi data downloaded from NYC TLC website:')
+# st.write(Ingest.data.head(5))
 
-#    # Create target variable
-#     def create_target_variable(self):
-#         self.data.with_columns((pl.col("tpep_dropoff_datetime") - pl.col("tpep_pickup_datetime")).alias("trip_duration"))
-    
 
 
 
